c49e1fc9f9c35e44a7a9bab39a6260fd3362de6a
sirx2713 <bradx2713@gmail.com>
2024-03-13 02:35:44 +0300

    Menu Application

---
+++ Menu_D21/MenuItem.swift
@@ -0,0 +1,17 @@
+//
+//  MenuItem.swift
+//  Menu_D21
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/13.
+//
+
+import Foundation
+import SwiftUI
+
+struct MenuItem: Identifiable{
+    
+    var id: UUID = UUID()
+    var name:String
+    var price:String
+    var imageName:Image
+}
---
+++ Menu_D21/Menu_D21App.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_D21App.swift
+//  Menu_D21
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/13.
+//
+
+import SwiftUI
+
+@main
+struct Menu_D21App: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
